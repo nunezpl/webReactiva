f637c5baa6cd3a1cd2090bb34451536f610921e8
NUÑEZ PAVA LORENA <134308403+nunezpl@users.noreply.github.com>
2023-10-24 14:33:38 -0500

    Delete taller3 directory

--- taller3/src/main/java/com/reactiva/taller3/Repositorio/RepositorioMateria.java
+++
@@ -1,9 +0,0 @@
-package com.reactiva.taller3.Repositorio;
-
-import com.reactiva.taller3.Modelo.Materia;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioMateria extends ReactiveCrudRepository<Materia, Integer> {
-}
--- taller3/src/main/java/com/reactiva/taller3/Repositorio/RepositorioNota.java
+++
@@ -1,9 +0,0 @@
-package com.reactiva.taller3.Repositorio;
-
-import com.reactiva.taller3.Modelo.Nota;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioNota extends ReactiveCrudRepository<Nota, Integer> {
-}
